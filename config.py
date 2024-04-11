a581ecca9074db57b522dc43cbdf379f6d1e78cf
Balapriyan B <131746104+BalaPriyan@users.noreply.github.com>
2024-04-11 12:33:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,19 +1,19 @@
 import os
 
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-API_ID = int(os.environ.get("API_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
-OWNER = int(os.environ.get("OWNER", ""))
-BOT_USERNAME = os.environ.get('BOT_USERNAME', "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7003249367:AAHmgua9CmH7Cw81gHVdOFeMobX1lFt8I2Y")
+API_ID = int(os.environ.get("API_ID", "27475576"))
+API_HASH = os.environ.get("API_HASH", "12b16abd69249bfb65633668a7cc416a")
+OWNER = int(os.environ.get("OWNER", "6501090288"))
+BOT_USERNAME = os.environ.get('BOT_USERNAME', "TomenRenamerBot")
 
-FORCE_SUBS = os.environ.get("FORCE_SUBS", "")
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+FORCE_SUBS = os.environ.get("FORCE_SUBS", "TomenBots")
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002114081913"))
 
-DB_URL = os.environ.get("DB_URL", "")
-DB_NAME = os.environ.get("DB_NAME", "madflixbotz")
+DB_URL = os.environ.get("DB_URL", "mongodb+srv://fino:fino@cluster0.ko0stef.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_NAME = os.environ.get("DB_NAME", "cluster0")
 
-STRING = os.environ.get("STRING", "")
+STRING = os.environ.get("STRING", "https://graph.org/file/a58f29d3d0d7acd886ff3.jpg")
 BOT_PIC = os.environ.get("BOT_PIC", "https://graph.org/file/ad48ac09b1e6f30d2dae4.jpg")
 
 
